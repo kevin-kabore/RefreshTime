30f5e058979ba7e22e321fec4079451aef4e665a
Kevin Kabore <kevinkabore@kevins-macbook-pro.local>
2016-03-20 20:41:58 -0400

    Initial Commit

---
+++ RefreshTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshTime
+//
+//  Created by Kevin Kabore on 3/20/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RefreshTime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RefreshTime
+//
+//  Created by Kevin Kabore on 3/20/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
